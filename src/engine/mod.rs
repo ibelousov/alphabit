f6fb75e25123060534f8c3e7b70a3058121e5acf
ibelousov <i.a.belousov.88@gmail.com>
2023-01-08 17:59:49 +0300

    initial commit

---
+++ src/engine/mod.rs
@@ -0,0 +1,349 @@
+use std::borrow::BorrowMut;
+use rand::Rng;
+use std::cell::RefCell;
+use std::ops::Index;
+use std::fs;
+
+pub enum Direction {
+    LEFT_TO_RIGHT,
+    RIGHT_TO_LEFT,
+    UP_TO_DOWN,
+    DOWN_TO_UP,
+    UP_LEFT_TO_DOWN_RIGHT,
+    DOWN_LEFT_TO_UP_RIGHT,
+    UP_RIGHT_TO_DOWN_LEFT,
+    DOWN_RIGHT_TO_UP_LEFT,
+    NONE
+}
+
+#[derive(Copy, Clone)]
+pub struct Ceil {
+    pub letter: char,
+    pub checked: i32
+}
+
+pub struct Field {
+    pub scores: RefCell<i32>,
+    is_word_ready: RefCell<bool>,
+    most_lengthy_word: RefCell<String>,
+    height: i32,
+    width: i32,
+    data: RefCell<Vec<Vec<Ceil>>>
+}
+
+impl  Field {
+    pub fn new(width: i32, height: i32) -> Self {
+        let data: RefCell<Vec<Vec<Ceil>>> = RefCell::new(Vec::new());
+
+        for y in (0..height) {
+            let mut nvec: Vec<Ceil> = Vec::new();
+
+            for x in (0..width) {
+                nvec.push(Ceil {
+                    letter: ' ',
+                    checked: 0
+                })
+            }
+
+            data.borrow_mut().push(nvec);
+        }
+
+        Field {
+            scores: RefCell::new(0),
+            is_word_ready: RefCell::new(false),
+            width,
+            height,
+            data,
+            most_lengthy_word: RefCell::new(String::new())
+        }
+    }
+
+    pub fn get_width(&self) -> i32 {
+        self.width
+    }
+
+    pub fn get_height(&self) -> i32 {
+        self.height
+    }
+
+    pub fn get_scores(&self) -> i32 {
+        *self.scores.borrow()
+    }
+
+    pub fn get_longest_word(&self) -> String {
+        self.most_lengthy_word.borrow().clone()
+    }
+
+    pub fn set_longest_word(&self, word: String) {
+        *self.most_lengthy_word.borrow_mut() = word;
+    }
+
+    pub fn set_scores(&self, scores: i32) {
+        let mut scorestochange = self.scores.borrow_mut();
+        *scorestochange = scores;
+    }
+
+    pub fn generate(&self) {
+        let string = String::from("АААААААААААААААААААААБББББВВВВВВВВВВВВВВВВГГГГГГДДДДДДДДДДЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЁЖЖЖЗЗЗЗЗИИИИИИИИИИИИИИИИИИИИИИИИИИИЙЙЙЙККККККККККККЛЛЛЛЛЛЛЛЛЛЛЛЛЛМММММММММММНННННННННННННННННННННННОООООООООООООООООООООООООООООООООООООППППППППППРРРРРРРРРРРРРРРРРСССССССССССССССССССТТТТТТТТТТТТТТТТТТТТТТУУУУУФФХХХЦЦЧЧЧЧЧШШЩЪЫЫЫЫЫЫЫЬЬЬЬЬЬЭЮЮЯЯЯЯЯЯЯ");
+
+        for j in (0..self.height) {
+            for i in (0..self.width) {
+                let mut random_index = rand::thread_rng().gen_range(0, string.chars().enumerate().count());
+
+                for (sz, ch) in string.chars().enumerate() {
+                    if random_index == 0 {
+                        self.data.borrow_mut()[j as usize][i as usize] = Ceil {
+                            checked: 0,
+                            letter: ch
+                        };
+                        // print!("{}", self.data.borrow_mut()[j as usize][i as usize].letter);
+                        break;
+                    }
+                    random_index -= 1;
+                }
+            }
+            // print!("\n");
+        }
+    }
+
+    pub fn is_blocked(&self) -> bool {
+        for i in (0..self.height) {
+            for j in (0..self.width) {
+                if self.get(j,i).checked < 0 {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
+    pub fn print(&self) {
+        for j in (0..self.height) {
+            for i in (0..self.width) {
+                let letter = self.data.borrow()[j as usize][i as usize].letter;
+                let checked = self.data.borrow()[j as usize][i as usize].checked;
+
+                if checked != 0 {
+                    print!("{}({}) ", letter, 'x');
+                } else {
+                    print!("{}({}) ", letter, '_');
+                }
+            }
+            print!("{}", "\n");
+        }
+    }
+
+    pub fn get(&self, x: i32, y: i32) -> Ceil {
+        self.data.borrow()[y as usize][x as usize]
+    }
+
+    pub fn set(&self, x: i32, y: i32, ceil: Ceil) {
+        self.data.borrow_mut()[y as usize][x as usize] = ceil;
+    }
+
+    pub fn is_word(&self) -> bool {
+        *self.is_word_ready.borrow()
+    }
+
+    pub fn check_word(&self, word: String) -> bool {
+
+        let dictionary = include_str!("../../assets/words.txt").split("\n");
+
+        let word2 = word.replace("е","ё");
+
+        for word_from_dictionary in dictionary {
+
+            let normalized_word = word_from_dictionary.trim().to_string().to_lowercase();
+
+            if normalized_word.eq(&word) || normalized_word.eq(&word2) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    pub fn is_checked(&self, x: i32, y: i32) -> i32 {
+        if x > self.width - 1 || y > self.height - 1 || x < 0 || y < 0 {
+            return -1;
+        }
+
+        self.data.borrow()[y as usize][x as usize].checked
+    }
+
+    pub fn find_max(&self) -> (i32,i32,i32) {
+        let mut max: i32 = 0;
+        let mut max_x: i32 = -1;
+        let mut max_y: i32 = -1;
+
+        for i in (0..self.height) {
+            for j in (0..self.width) {
+                if max < self.data.borrow()[i as usize][j as usize].checked {
+                    max = self.data.borrow()[i as usize][j as usize].checked;
+                    max_x = j;
+                    max_y = i;
+                }
+            }
+        }
+
+        (max,max_x,max_y)
+    }
+
+    pub fn down(&self, x: i32, mut y: i32) {
+        let mut data = self.data.borrow_mut();
+
+        let mut y_n = y - 1;
+        while y_n > -1 && data[y_n as usize][x as usize].checked >= 0 {
+            let temp = data[y as usize][x as usize];
+            data[y as usize][x as usize] = data[y_n as usize][x as usize];
+            data[y_n as usize][x as usize] = temp;
+            y_n -= 1;
+            y -= 1;
+        }
+    }
+
+    pub fn deselect(&self, x: i32, y: i32) {
+        let mut data = self.data.borrow_mut();
+
+        for i in (0..self.height) {
+            for j in (0..self.width) {
+                data[i as usize][j as usize] = Ceil {
+                    checked: 0,
+                    letter: data[i as usize][j as usize].letter
+                };
+            }
+        }
+    }
+
+    pub fn try_check(&self, x: i32, y: i32) {
+
+        let (max_val,max_x,max_y) = self.find_max();
+
+        let word = self.get_word();
+        let word2 = word.clone();
+        let word3 = word.clone();
+
+        let mut data = self.data.borrow_mut();
+
+        let is_checked = data[y as usize][x as usize].checked == 0;
+        let is_exists = max_val > 0;
+        let is_previous = (data[y as usize][x as usize].checked == max_val - 1) && data[y as usize][x as usize].checked > 0;
+        let is_near = ((max_x - x).abs() == 1 || (max_y - y).abs() == 1) &&
+                            ((max_x - x).abs() <= 1 && (max_y - y).abs() <= 1) &&
+                            is_checked;
+        let is_same = (x == max_x) && (y == max_y);
+
+        if is_near {
+            data[y as usize][x as usize] = Ceil {
+                checked: max_val + 1,
+                letter:data[y as usize][x as usize].letter
+            };
+        } else if !is_exists {
+            data[y as usize][x as usize] = Ceil {
+                checked: 1,
+                letter: data[y as usize][x as usize].letter
+            };
+        } else if is_same && self.check_word(word.to_lowercase()) {
+
+            for y in (0..self.height) {
+                for x in (0..self.width) {
+                    if data[y as usize][x as usize].checked > 0 {
+                        data[y as usize][x as usize] = Ceil {
+                            letter: data[y as usize][x as usize].letter,
+                            checked: -253
+                        };
+                    }
+                }
+            }
+
+            if self.most_lengthy_word.borrow_mut().len() < word2.len() {
+                *self.most_lengthy_word.borrow_mut() = word2.clone();
+            }
+
+            let scores = self.scores.take() + ((word2.capacity() / 2) * (word2.capacity() / 2)) as i32;
+            self.set_scores(scores);
+        }
+
+        drop(data);
+
+        let word = self.get_word();
+        *self.is_word_ready.borrow_mut() = self.check_word(word.to_lowercase());
+    }
+
+    pub fn get_at_value(&self, val: i32) -> char {
+        for i in (0..self.height) {
+            for j in (0..self.width) {
+                if self.get(j,i).checked == val {
+                    return self.get(j,i).letter;
+                }
+            }
+        }
+
+        'x'
+    }
+
+    pub fn get_word(&self) -> String {
+        let mut value = String::from("");
+        let (max_val,max_x, max_y) = self.find_max();
+
+        for i in (1..max_val+1) {
+            value.push(self.get_at_value(i));
+        }
+
+        value
+    }
+
+    pub fn get_direction(&self, x: i32, y: i32) -> Direction {
+
+        let v = self.data.borrow()[y as usize][x as usize].checked;
+
+        if v > 0 {
+            if self.is_checked(x+1,y) == v + 1 {
+                return Direction::LEFT_TO_RIGHT;
+            } else if self.is_checked(x-1,y) == v + 1 {
+                return Direction::RIGHT_TO_LEFT;
+            } else if self.is_checked(x,y-1) == v + 1 {
+                return Direction::DOWN_TO_UP;
+            } else if self.is_checked(x,y+1) == v + 1 {
+                return Direction::UP_TO_DOWN;
+            } else if self.is_checked(x-1,y+1) == v + 1 {
+                return Direction::UP_RIGHT_TO_DOWN_LEFT;
+            } else if self.is_checked(x-1,y-1) == v + 1 {
+                return Direction::DOWN_RIGHT_TO_UP_LEFT;
+            } else if self.is_checked(x+1,y+1) == v + 1 {
+                return Direction::UP_LEFT_TO_DOWN_RIGHT;
+            } else if self.is_checked(x+1,y-1) == v + 1 {
+                return Direction::DOWN_LEFT_TO_UP_RIGHT;
+            }
+        }
+
+        Direction::NONE
+    }
+
+    pub fn get_before_direction(&self, x: i32, y: i32) -> Direction {
+        let v = self.data.borrow()[y as usize][x as usize].checked;
+
+        if v > 1 {
+            if self.is_checked(x+1,y) == v - 1 {
+                return Direction::LEFT_TO_RIGHT;
+            } else if self.is_checked(x-1,y) == v - 1 {
+                return Direction::RIGHT_TO_LEFT;
+            } else if self.is_checked(x,y-1) == v - 1 {
+                return Direction::DOWN_TO_UP;
+            } else if self.is_checked(x,y+1) == v - 1 {
+                return Direction::UP_TO_DOWN;
+            } else if self.is_checked(x-1,y+1) == v - 1 {
+                return Direction::UP_RIGHT_TO_DOWN_LEFT;
+            } else if self.is_checked(x-1,y-1) == v - 1 {
+                return Direction::DOWN_RIGHT_TO_UP_LEFT;
+            } else if self.is_checked(x+1,y+1) == v - 1 {
+                return Direction::UP_LEFT_TO_DOWN_RIGHT;
+            } else if self.is_checked(x+1,y-1) == v - 1 {
+                return Direction::DOWN_LEFT_TO_UP_RIGHT;
+            }
+        }
+
+        Direction::NONE
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,280 @@
+mod engine;
+
+use std::error::Error;
+use std::borrow::{Borrow, BorrowMut};
+use engine::*;
+use fltk::{app, button::Button, frame::Frame, prelude::*, *, draw::*};
+use fltk::window::Window;
+use fltk::enums::{Color, Event, FrameType};
+use std::sync::{mpsc, Mutex};
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::thread;
+use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
+use serde::{Serialize, Deserialize};
+use csv::ByteRecord;
+use fltk::app::MouseButton;
+use fltk::image::Image;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    longest_name: String,
+    leader_table: Vec<(String,String,String)>,
+    saved_field: Vec<Vec<char>>,
+    saved_longest_name: String,
+    saved_scores: i32,
+    position_x: i32,
+    position_y: i32
+}
+
+impl ::std::default::Default for Config {
+    fn default() -> Self {
+        Self {
+            longest_name: String::new(),
+            leader_table: Vec::new(),
+            saved_field: Vec::new(),
+            saved_longest_name: String::new(),
+            saved_scores: 0,
+            position_x: 0,
+            position_y: 0
+        }
+    }
+}
+
+const WIDTH: i32 = 15;
+const HEIGHT: i32 = 15;
+const TITLE: &str = "Альфабит";
+const OFFSET_Y: i32 = 40;
+const SETTINGS_NAME: &str = "alphabitsettings";
+
+fn get_green() -> i32 {
+    let millis = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis();
+
+    let millis: i32 = (millis % 1000) as i32;
+    let millis =  (millis - 500).abs();
+
+    68 + (millis / 10)
+}
+
+fn draw_direction(j:i32, i:i32, direction:Direction)
+{
+    set_line_style(LineStyle::Solid, 3);
+
+    match direction {
+        Direction::LEFT_TO_RIGHT => {
+            draw_line(j * 40 + 20, i * 40 + 20 + OFFSET_Y, (j + 1) * 40, i * 40 + 20 + OFFSET_Y);
+        },
+        Direction::RIGHT_TO_LEFT => {
+            draw_line(j * 40 + 20, i * 40 + 20 + OFFSET_Y, j * 40, i * 40 + 20 + OFFSET_Y);
+        },
+        Direction::UP_TO_DOWN => {
+            draw_line(j * 40 + 20, i * 40 + 20 + OFFSET_Y, j * 40 + 20, (i + 1) * 40 + OFFSET_Y);
+        },
+        Direction::DOWN_TO_UP => {
+            draw_line(j * 40 + 20, i * 40 + 20 + OFFSET_Y,j * 40 + 20, i * 40 + OFFSET_Y);
+        },
+        Direction::DOWN_RIGHT_TO_UP_LEFT => {
+            draw_line(j * 40 + 20, i * 40 + 20 + OFFSET_Y, j * 40, i * 40 + OFFSET_Y);
+        },
+        Direction::UP_RIGHT_TO_DOWN_LEFT => {
+            draw_line(j * 40 + 20, i * 40 + 20 + OFFSET_Y,j * 40, (i+1) * 40 + OFFSET_Y);
+        },
+        Direction::DOWN_LEFT_TO_UP_RIGHT => {
+            draw_line(j * 40 + 20, i * 40 + 20 + OFFSET_Y, (j+1) * 40, i * 40 + OFFSET_Y);
+        },
+        Direction::UP_LEFT_TO_DOWN_RIGHT => {
+            draw_line(j * 40 + 20, i * 40 + 20 + OFFSET_Y,(j + 1) * 40, (i + 1) * 40 + OFFSET_Y);
+        },
+        Direction::NONE => {}
+    }
+}
+
+fn main() -> Result<(), confy::ConfyError> {
+
+    let mut config: Config = confy::load(SETTINGS_NAME, None)?;
+
+    let app = app::App::default();
+
+    let mut field = Rc::new(Field::new(WIDTH,HEIGHT));
+    field.borrow_mut().generate();
+    field.borrow_mut().set_longest_word(config.saved_longest_name.clone());
+    field.borrow_mut().set_scores(config.saved_scores);
+
+    for (y, row) in config.saved_field.iter().enumerate() {
+        for (x,col) in row.iter().enumerate() {
+            // println!("x: {}, y: {}", x, y);
+            field.borrow_mut().set(x as i32,y as i32, Ceil { letter: *col, checked: 0})
+        }
+    }
+
+    let field_draw = Rc::clone(&field);
+    let field_animation = Rc::clone(&field);
+
+    let mut wind = Window::new(config.position_x, config.position_y, WIDTH * 40, HEIGHT * 40 + OFFSET_Y, TITLE);
+
+    let image = image::PngImage::from_data(
+        include_bytes!("../assets/icon.png")
+    ).unwrap();
+    wind.set_icon(Some(image));
+
+    wind.handle({
+        move |f, ev| {
+            match ev {
+                Event::Push => {
+                    if field.is_blocked() {
+                       return true;
+                    }
+
+                    let (x,y) = app::event_coords();
+
+                    if y > 5 && y < OFFSET_Y - 10 && x > 455 && x < 600 {
+                        field.generate();
+                        field.set_longest_word(String::new());
+                        field.set_scores(0);
+                        f.redraw();
+
+                        config.borrow_mut().saved_longest_name = String::new();
+                        config.borrow_mut().saved_field = Vec::new();
+                        config.borrow_mut().saved_scores = 0;
+                        confy::store(SETTINGS_NAME, None, &config);
+                    }
+
+                    if y < OFFSET_Y {
+                        return true;
+                    }
+
+                    let (cell_x, cell_y) = (x / 40, (y - OFFSET_Y) / 40);
+
+                    if app::event_mouse_button() == MouseButton::Right {
+                        field.deselect(cell_x, cell_y);
+                    } else {
+                        field.try_check(cell_x, cell_y);
+                    }
+
+                    let bconfig = config.borrow_mut();
+                    bconfig.saved_longest_name = field.get_longest_word();
+                    bconfig.saved_scores = field.get_scores();
+
+                    confy::store(SETTINGS_NAME, None, &config);
+
+                    f.redraw();
+
+                    true
+                },
+                Event::NoEvent => {
+
+                    if config.borrow_mut().position_x != f.x() ||
+                        config.borrow_mut().position_x != f.y() {
+                        config.borrow_mut().position_x = f.x();
+                        config.borrow_mut().position_y = f.y();
+                        confy::store(SETTINGS_NAME, None, &config);
+                    }
+
+                    let bconfig = config.borrow_mut();
+                    bconfig.saved_field = Vec::new();
+                    for y in (0..HEIGHT) {
+                        let mut row = Vec::new();
+
+                        for x in (0..WIDTH) {
+                            row.push(field.get(x,y).letter);
+                        }
+
+                        bconfig.saved_field.push(row);
+                    }
+
+                    true
+                },
+                _ => false
+            }
+        }
+    });
+
+    wind.draw(move |w| {
+
+        set_font(enums::Font::Courier, 16);
+        draw_rect_fill( 0,0,40 * WIDTH, OFFSET_Y,enums::Color::rgb_color(118,50, 40));
+        set_draw_color(enums::Color::rgb_color(255,255,255));
+        draw_text( &format!("ОЧКИ: {:0>5}", field_draw.get_scores()), 15, 25);
+
+        draw_rect_fill(150,0,300, OFFSET_Y, enums::Color::rgb_color(0,140,210));
+        set_draw_color(enums::Color::rgb_color(255,255,255));
+        if field_draw.get_longest_word().len() > 0 {
+            draw_text( &format!("{:^25}", field_draw.get_longest_word().to_uppercase()), 175, 25);
+        } else {
+            draw_text( &format!("{:^25}", "-"), 175, 25);
+        }
+
+        draw_rect_fill(450,0,150, OFFSET_Y, enums::Color::rgb_color(50,200,160));
+        set_draw_color(enums::Color::rgb_color(255,255,255));
+        draw_text( &format!("{}", "ФИНИШ"), 500, 25);
+        let green = get_green() as u8;
+        let color = if field_draw.is_word() == true {
+            enums::Color::rgb_color(100, green,100)
+        } else {
+            enums::Color::rgb_color(100, 100,100)
+        };
+
+        draw_rect_fill(0, OFFSET_Y, WIDTH * 40, HEIGHT * 40, enums::Color::rgb_color(40,42,54));
+
+        for i in (0..HEIGHT) {
+            for j in (0..WIDTH) {
+                let checked_value = field_draw.is_checked(j, i);
+
+                if checked_value > 0 {
+
+                    draw_rect_fill(j * 40 + 1, i * 40 + 1 + OFFSET_Y, 38, 38, color);
+
+                    set_draw_color(enums::Color::White);
+                    draw_text( &format!("{}", field_draw.get(j,i).letter), j * 40 + 15, i * 40 + 25 + OFFSET_Y);
+
+                    set_draw_color(enums::Color::White);
+                    draw_direction(j,i,field_draw.get_before_direction(j,i));
+                    draw_direction(j,i,field_draw.get_direction(j,i));
+
+                } else if checked_value == 0 {
+                    draw_rect_fill(j * 40 + 1, i * 40 + 1 + OFFSET_Y, 38, 38, enums::Color::rgb_color(63,65,82));
+                    set_draw_color(enums::Color::rgb_color(230,230,230));
+
+                    draw_text( &format!("{}", field_draw.get(j,i).letter), j * 40 + 15, i * 40 + 25 + OFFSET_Y);
+                } else {
+                    let alpha = (checked_value.abs() as u8) / 5;
+                    draw_rect_fill(j * 40 + 1, i * 40 + 1 + OFFSET_Y, 38, 38, color);
+                    draw_rect_fill(j * 40 + 1, i * 40 + 1 + OFFSET_Y, 38, 38, enums::Color::rgb_color(63+alpha,65+alpha,82+alpha));
+                    set_draw_color(enums::Color::rgb_color(163+alpha,165+alpha,182+alpha));
+
+                    if(checked_value == -1) {
+                        field_draw.set(j, i, Ceil {
+                            checked: 0,
+                            letter: ' '
+                        });
+                        field_draw.down(j,i);
+                    } else {
+                        field_draw.set(j, i, Ceil {
+                            checked: checked_value + 14,
+                            letter: field_draw.get(j, i).letter,
+                        });
+                    }
+
+                    draw_text( &format!("{}", field_draw.get(j,i).letter), j * 40 + 15, i * 40 + 25 + OFFSET_Y);
+
+                }
+            }
+        }
+    });
+
+    wind.end();
+    wind.show();
+
+    app::add_idle3(move |_| {
+        app::sleep(0.01);
+        wind.redraw();
+    });
+
+    app.run().unwrap();
+
+    Ok(())
+}
